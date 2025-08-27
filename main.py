53c9d2fa8d594adb6fd0e6a568daf6ddbddf2319
CarlosEZaul <carlosezzddomingos@gmail.com>
2025-08-27 20:31:04 -0300

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+ref_arq = open("pessoas.txt","r") 
+for linha in ref_arq: 
+    itens = linha.split()
+    print("Nome:", itens[0], itens[1])
+    print("Endereço: ", itens[2], itens[3], itens[4])
+    print("Documento de Identidade: ", itens[5])
+    print("Data de Nascimento: ", itens[6])
+    print("\n\n")
+ref_arq.close()
